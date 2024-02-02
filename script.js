32a035913150a34234c57b239bdf1b863efa3e49
Andre Cunningham <154343341+MrDreKC@users.noreply.github.com>
2024-02-02 12:55:41 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const nextBirthday = new Date(`November 30 ${currentYear} 00:00:00`);
+
+// Countdown Timer
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = nextBirthday - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000/ 60 / 60) % 24;
+  const m = Math.floor(diff / 1000/ 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
+
